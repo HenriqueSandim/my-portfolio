d9731c3e7f0c15dc1bed8afdb25bf4ec0abb36e2
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-05-05 13:53:05 -0300

    add: deploy link dinamyc generation

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -11,6 +11,7 @@ import { Text } from "@/styles/Text";
 import { useEffect, useState } from "react";
 import { FaGithub, FaShare } from "react-icons/fa";
 import { userData } from "@/utils/userData";
+import { deployLinks } from "@/utils/projectsDeploy";
 
 interface ReposType {
   id: number;
@@ -43,54 +44,58 @@ export const Project = (): JSX.Element => {
   return (
     <>
       {repositories &&
-        repositories?.map?.((repository) => (
-          <ProjectWrapper key={repository.id}>
-            <ProjectTitle
-              as="h2"
-              type="heading3"
-              css={{ marginBottom: "$3" }}
-              color="grey4"
-            >
-              {repository.name}
-            </ProjectTitle>
+        repositories?.map?.((repository) => {
+          const deployLink = deployLinks[repository.name as keyof Object]
 
-            <ProjectStack>
-              <Text type="body2" color="grey2">
-                Primary Language:
+          return (
+            <ProjectWrapper key={repository.id}>
+              <ProjectTitle
+                as="h2"
+                type="heading3"
+                css={{ marginBottom: "$3" }}
+                color="grey4"
+              >
+                {repository.name}
+              </ProjectTitle>
+
+              <ProjectStack>
+                <Text type="body2" color="grey2">
+                  Primary Language:
+                </Text>
+                {repository.language ? (
+                  <ProjectStackTech>
+                    <Text color="grey2" type="body2">
+                      {repository.language}
+                    </Text>
+                  </ProjectStackTech>
+                ) : (
+                  <ProjectStackTech>
+                    <Text color="grey2" type="body2">
+                      Primary language not identified
+                    </Text>
+                  </ProjectStackTech>
+                )}
+              </ProjectStack>
+
+              <Text type="body1" color="grey2">
+                {repository.description?.substring(0, 129)}
               </Text>
-              {repository.language ? (
-                <ProjectStackTech>
-                  <Text color="grey2" type="body2">
-                    {repository.language}
-                  </Text>
-                </ProjectStackTech>
-              ) : (
-                <ProjectStackTech>
-                  <Text color="grey2" type="body2">
-                    Primary language not identified
-                  </Text>
-                </ProjectStackTech>
-              )}
-            </ProjectStack>
-
-            <Text type="body1" color="grey2">
-              {repository.description?.substring(0, 129)}
-            </Text>
-            <ProjectLinks>
-              <ProjectLink target="_blank" href={repository.html_url}>
-                <FaGithub /> Github Code
-              </ProjectLink>
-              {repository.homepage && (
-                <ProjectLink
-                  target="_blank"
-                  href={`https://${repository.homepage}`}
-                >
-                  <FaShare /> See demo
+              <ProjectLinks>
+                <ProjectLink target="_blank" href={repository.html_url}>
+                  <FaGithub /> Github Code
                 </ProjectLink>
-              )}
-            </ProjectLinks>
-          </ProjectWrapper>
-        ))}
+                {deployLink && (
+                  <ProjectLink
+                    target="_blank"
+                    href={`https://${deployLink}`}
+                  >
+                    <FaShare /> See demo
+                  </ProjectLink>
+                )}
+              </ProjectLinks>
+            </ProjectWrapper>
+          )}
+        )}
     </>
   );
 };
---
+++ src/utils/projectsDeploy.ts
@@ -0,0 +1,9 @@
+interface IDeployLinks {
+    "port-geek": string,
+    "grupo16-t13-felipe-Projeto_FullStack": string
+}
+
+export const deployLinks: IDeployLinks = {
+    "port-geek": "portgeek.vercel.app/",
+    "grupo16-t13-felipe-Projeto_FullStack": "projeto-full-stack-ashy.vercel.app/"
+}
